8abf1652cdded06dafe70a78cd1c6ca9e56dd4ab
Scoperr <ambaldhagetarun@gmail.com>
2024-07-20 17:48:41 -0400

    mlflow experiments registered

--- src/utils.py
+++ src/utils.py
@@ -5,8 +5,9 @@ import os
 import numpy as np
 import pandas as pd
 import dill
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+import mlflow
+from mlflow.models import infer_signature
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -24,29 +25,46 @@ def evaluate_models(X_train,X_test,y_train,y_test,models):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
+        
+        MLFLOW_TRACKING_URI = "https://dagshub.com/ambaldhagetarun/Heart.mlflow"
+        os.environ['MLFLOW_TRACKING_USERNAME'] = 'ambaldhagetarun'
+        os.environ['MLFLOW_TRACKING_PASSWORD'] = 'd9df774016ce00b3f7146677dd7233df6315b317'
+
+        mlflow.set_tracking_uri(uri=MLFLOW_TRACKING_URI)
+        mlflow.set_experiment("Heart_Attack_test")
+
+        for name,model in models.items():
             
-            print(model)
-            # gs = GridSearchCV(model, para, cv=3, scoring='accuracy')
-            # gs.fit(X_train, y_train) 
+            with mlflow.start_run(run_name=name):
+                print(name)
+                model.fit(X_train, y_train)
             
-            # model.set_params(**gs.best_params_)
-            model.fit(X_train, y_train)
+                # Make Predictions
+                y_test_pred = model.predict(X_test)
+                
+                test_model_score = accuracy_score(y_test, y_test_pred)
+                
+                report[name] = test_model_score
+
+                
+                mlflow.log_params(model.get_params())
+                mlflow.log_metric("accuracy_score",test_model_score)
+                
+                signature = infer_signature(X_train, model.predict(X_train))
+
+                mlflow.sklearn.log_model(
+                    sk_model=model,
+                    artifact_path="model",
+                    signature=signature,
+                    input_example=X_train,
+                    registered_model_name=name
+                )
+
+                logging.info(f"Model: {name}")
+                logging.info(f"Test Accuracy: {test_model_score}\n")
             
-            # Make Predictions
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+
             
-            train_model_score = accuracy_score(y_train, y_train_pred)
-            test_model_score = accuracy_score(y_test, y_test_pred)
-            
-            report[list(models.keys())[i]] = test_model_score
-            
-            logging.info(f"Model: {list(models.keys())[i]}")
-            # logging.info(f"Best Parameters: {gs.best_params_}")
-            logging.info(f"Train Accuracy: {train_model_score}")
-            logging.info(f"Test Accuracy: {test_model_score}\n")
                 
         return report
     except Exception as e:
